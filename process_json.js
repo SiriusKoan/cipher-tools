e7f4d26f6248c25b4b2e96d59c98eadd27f32687
SiriusKoan <k.peihsun@gmail.com>
2023-04-28 15:04:33 +0800

    Remove beautify json

--- process_json.js
+++
@@ -1,4 +0,0 @@
-json = document.body.innerText;
-var json_block = document.createElement("pre");
-json_block.innerText = JSON.stringify(JSON.parse(json), null, 4);
-document.body.appendChild(json_block);
